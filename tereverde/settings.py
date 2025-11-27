3b0cac8e1cbab2d0d1f43c5fd3efa765375ee5ea
Anderson Macharett <165483617+Macharett@users.noreply.github.com>
2025-11-26 21:11:06 -0300

    Atualizando Banco para Postgree

--- tereverde/settings.py
+++ tereverde/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tereverde.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'EjMGzAGkRNDIYRdffWUJQjXWwVPrFOaX',
+        'HOST': 'switchback.proxy.rlwy.net',
+        'PORT': '45610',
     }
 }
 
